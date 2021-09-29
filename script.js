e71ed89f35d1d7a43cf23727446553b03765f05b
Tommy Tran <tommytran.developer@gmail.com>
2021-09-29 22:53:49 +1000

    Mobile navbar with JS

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-
-
-
-
-
-
-
-
-
-
 let burger = document.querySelector(".burger");
 let list = document.querySelector(".navList");
 let body = document.querySelector("body");
 let ul = document.querySelector("ul")
 
 burger.addEventListener('click', function(event) {
-    list.classList.add('navList')
     burger.classList.toggle('toggle');
+    list.classList.toggle('nav-active');
 })
